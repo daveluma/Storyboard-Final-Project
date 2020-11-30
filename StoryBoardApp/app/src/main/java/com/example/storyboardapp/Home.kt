1a387462ac183656e4a7c20dc69af73bff2d2e74
ashan399 <ashan399@gmail.com>
2020-11-30 18:13:10 -0500

    updated home page

--- StoryBoardApp/app/src/main/java/com/example/storyboardapp/Home.kt
+++ StoryBoardApp/app/src/main/java/com/example/storyboardapp/Home.kt
@@ -1,9 +1,17 @@
 package com.example.storyboardapp
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
 import android.widget.Button
+import android.widget.GridView
+import android.widget.TextView
 import com.example.myapplication.BoardActivity
 import com.example.postactivity.WritePostActivity
 
@@ -11,7 +19,9 @@ class Home : AppCompatActivity() {
     private lateinit var createPost : Button
     private lateinit var boardPost : Button
     private lateinit var dashboard : Button
-
+    private lateinit var gridView: GridView
+    private lateinit var images: IntArray;
+//    private lateinit var titles: Array<String>;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -33,5 +43,58 @@ class Home : AppCompatActivity() {
         dashboard.setOnClickListener {
             startActivity(Intent(this, FeedActivity::class.java))
         }
+
+
+        // dynamic grid implementation https://www.youtube.com/watch?v=RtitGGmvvTI
+        gridView = findViewById(R.id.gridView)
+        gridView.adapter = CustomAdapter(this)
+        // replace contents of intArrayOf(...) with user's images
+        images = intArrayOf(R.drawable.ic_launcher_background)
+//        titles = Array<String>(1);
+//        titles[0] = "test name"
+
+    }
+
+    // class for grid view adapter
+    public class CustomAdapter: BaseAdapter {
+        private lateinit var imageNames: Array<String>
+        private lateinit var imagePhotos: Array<String>
+        private lateinit var context: Context
+        private lateinit var layoutInflater: LayoutInflater
+
+//        constructor(imageNames: Array<String>, imagePhotos: Array<String>, context: Context) : super() {
+//            this.imageNames = imageNames
+//            this.imagePhotos = imagePhotos
+//            this.context = context
+//            this.layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as (LayoutInflater)
+//        }
+        constructor(context: Context) : super() {
+            this.context = context
+            this.layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as (LayoutInflater)
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val view = layoutInflater.inflate(R.layout.story_board_cover_view, parent, false)
+            val title = view.findViewById<TextView>(R.id.textViewCoverTitle)
+            title.text = "Sonic the HedgeHog lmao"
+            val description = view.findViewById<TextView>(R.id.textViewCoverDescription)
+            description.text = "Sonic the Hedgehog is a really cool movie you should totally see it"
+
+            return view
+
+        }
+
+        override fun getItem(position: Int): Any {
+            TODO("Get post from db")
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getCount(): Int {
+            return 20
+        }
+
     }
 }
--- StoryBoardApp/app/src/main/java/com/example/storyboardapp/MainActivity.kt
+++ StoryBoardApp/app/src/main/java/com/example/storyboardapp/MainActivity.kt
@@ -10,6 +10,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import java.net.Inet4Address
 
 
 // All register/login stuff from here https://www.youtube.com/watch?v=Z-RE1QuUWPg
@@ -22,65 +23,68 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //TODO Commented this out
         setContentView(R.layout.activity_main)
 
-        mAuth = FirebaseAuth.getInstance();
-
-        mButtonRegister = findViewById(R.id.buttonRegisterBackToMain)
-        mButtonLogin = findViewById(R.id.buttonSignIn)
-        mEditTextEmail = findViewById(R.id.editTextLoginEmail)
-        mEditTextPassword = findViewById(R.id.editTextLoginPassword)
-        mButtonRegister.setOnClickListener {
-            startActivity(Intent(this, RegisterUser::class.java))
-        }
-
-        mButtonLogin.setOnClickListener {
-            loginUserAccount()
-        }
+        startActivity(Intent(this, Home::class.java))
+//        mAuth = FirebaseAuth.getInstance();
+//
+//        mButtonRegister = findViewById(R.id.buttonRegisterBackToMain)
+//        mButtonLogin = findViewById(R.id.buttonSignIn)
+//        mEditTextEmail = findViewById(R.id.editTextLoginEmail)
+//        mEditTextPassword = findViewById(R.id.editTextLoginPassword)
+//        mButtonRegister.setOnClickListener {
+//            startActivity(Intent(this, RegisterUser::class.java))
+//        }
+//
+//        mButtonLogin.setOnClickListener {
+//            loginUserAccount()
+//        }
 
 
     }
 
-    private fun loginUserAccount() {
-
-        val email: String = mEditTextEmail.text.toString()
-        val password: String = mEditTextPassword.text.toString()
-
-        if (TextUtils.isEmpty(email)) {
-            mEditTextEmail.error = "please enter an email"
-            return
-        }
-        if (TextUtils.isEmpty(password)) {
-            mEditTextEmail.error = "Please enter a password"
-            return
-        }
-
-        mAuth!!.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    Toast.makeText(applicationContext, "Login successful!", Toast.LENGTH_LONG)
-                        .show()
-                    startActivity(Intent(this, Home::class.java))
-                } else {
-                    Toast.makeText(
-                        applicationContext,
-                        "Login failed! Please try again later",
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
-            }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser: FirebaseUser? = mAuth.currentUser
-        if (currentUser != null) {
-            // TODO Uncomment this, if user was already logged in before, automatically signs them in
-//            startActivity(Intent(this, Home::class.java))
-        }
-//        updateUI(currentUser)
-    }
+//    private fun loginUserAccount() {
+//
+//        val email: String = mEditTextEmail.text.toString()
+//        val password: String = mEditTextPassword.text.toString()
+//
+//        if (TextUtils.isEmpty(email)) {
+//            mEditTextEmail.error = "please enter an email"
+//            return
+//        }
+//        if (TextUtils.isEmpty(password)) {
+//            mEditTextEmail.error = "Please enter a password"
+//            return
+//        }
+//
+//        mAuth!!.signInWithEmailAndPassword(email, password)
+//            .addOnCompleteListener { task ->
+//                if (task.isSuccessful) {
+//                    Toast.makeText(applicationContext, "Login successful!", Toast.LENGTH_LONG)
+//                        .show()
+//                    startActivity(Intent(this, Home::class.java))
+//                } else {
+//                    Toast.makeText(
+//                        applicationContext,
+//                        "Login failed! Please try again later",
+//                        Toast.LENGTH_LONG
+//                    ).show()
+//                }
+//            }
+//    }
+//
+//    override fun onStart() {
+//        super.onStart()
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        val currentUser: FirebaseUser? = mAuth.currentUser
+//        if (currentUser != null) {
+//            // TODO Uncomment this, if user was already logged in before, automatically signs them in
+////            startActivity(Intent(this, Home::class.java))
+//        }
+////        updateUI(currentUser)
+//    }
 
 
 }
